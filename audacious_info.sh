9e0346b5fe9a8bebdd1bad32fabb80f06814c27e
pedherall <pedherall@gmail.com>
2019-06-27 19:17:58 +0200

    Update audacious_info.sh

--- audacious_info.sh
+++ audacious_info.sh
@@ -6,14 +6,12 @@ then
 {
 echo "<txt><span weight='normal' fgcolor='#C2C2C3'>$(audtool current-song)</span></txt>"
 echo "<tool><span size='large' weight='ultrabold'>$(audtool current-song-tuple-data album)</span>
+<span size='medium'>$(audtool current-song-tuple-data artist)
 
-$(audtool current-song-tuple-data artist)
+<span style='italic' >$(audtool current-song-tuple-data track-number) - $(audtool current-song-tuple-data title):</span> $(audtool current-song-output-length) / $(audtool current-song-length)
 
-$(audtool current-song-tuple-data track-number) - $(audtool current-song-tuple-data title): $(audtool current-song-output-length) / $(audtool current-song-length)
-
-$(audtool current-song-info)
-	 
-</tool>"
+$(audtool --current-song-tuple-data codec) $(audtool --current-song-tuple-data quality)
+Bitrate: $(audtool current-song-bitrate-kbps) Kbps   Frecuencia: $(audtool current-song-frequency-khz) Khz </span></tool>"
 echo "<txtclick>audtool mainwin-show</txtclick>"
 }
 
