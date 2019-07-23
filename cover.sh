82816269f8bac24ba73bf0710bdf3f8b0548f135
pedherall <pedherall@gmail.com>
2019-07-23 11:36:22 +0200

    Update cover.sh

--- cover.sh
+++ cover.sh
@@ -3,7 +3,6 @@
 if pgrep "audacious" > /dev/null
 then
 {
-#convert -thumbnail 24 "$(audtool current-song-tuple-data file-path)/cover*" "$(audtool current-song-tuple-data album)"
 echo "<img>/home/pedro/.cache/audacious/current.jpg</img>"
 echo "<tool>Pulsa para pausar</tool>"
 echo "<click>audtool  playback-playpause</click>"
